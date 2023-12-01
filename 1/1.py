0f15d4ab3a039075f0c54cb5ffae4771ce3c42de
Oskar Deubler <deubler.oskar@gmail.com>
2023-12-01 13:08:25 +0100

    First 1/2 Complete

--- 1/1.py
+++ 1/1.py
@@ -1,6 +1,29 @@
-print("Reading file")
-with open ('input.txt') as input:
-    lines = input.readlines()
 
-for x in lines:
-    print(x)  
+def read_file(filename):
+    with open (filename) as input:
+        lines = input.readlines()
+    input.close()
+    return lines
+
+def find_first_number(string):
+    for x in string:
+        if x.isdigit():
+            return x
+def get_calibration_value(string):
+    reverse_string = string[::-1]
+    first_num = find_first_number(string)
+    last_num = find_first_number(reverse_string)
+    number = first_num + last_num
+    return int(number)
+
+def main():
+
+    lines = read_file('input.txt')
+    sum = 0
+    for x in lines:
+        sum += get_calibration_value(x)
+    print(sum)
+    
+if __name__ == "__main__":
+    main()
+
