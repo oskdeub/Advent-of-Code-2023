4aacd3d410dbe2e832d2aab37c028408d9a83fe4
Oskar Deubler <deubler.oskar@gmail.com>
2023-12-01 23:12:27 +0100

    refactored a function into insert_number_after_head

--- 1/part2.py
+++ 1/part2.py
@@ -15,35 +15,33 @@ def get_calibration_value(string):
     last_num = find_first_number(reverse_string)
     number = first_num + last_num
     return int(number)
-        
+def insert_number_after_head(string, number):
+    return string[:1] + str(number) + string[1:]
+    
 def replace_spelled_numbers(string):
 
     num_dict = {
-        "one" : 1,
-        "two" : 2,
-        "three" : 3,
-        "four" : 4,
-        "five" : 5,
-        "six" : 6,
-        "seven" : 7,
-        "eight" : 8,
-        "nine" : 9
+        "one"   :   1,
+        "two"   :   2,
+        "three" :   3,
+        "four"  :   4,
+        "five"  :   5,
+        "six"   :   6,
+        "seven" :   7,
+        "eight" :   8,
+        "nine"  :   9
     }
     
-    #Find spelled numbers in string, add
-    found_dict = {}
+    #Find spelled numbers in string, insert their corresponding number into the substring 
     for x in num_dict.keys():
         if x in string:
-            nSubstring = x
-            nSubstring = nSubstring[:1] + str(num_dict[x]) + nSubstring[1:]
+            nSubstring = insert_number_after_head(x, num_dict[x])
             string = string.replace(x, nSubstring)
-            
-    print(string)
     return string
 
 def main():
     lines = read_file('input.txt')
-    #lines = ['oneight']
+    
     for x in range(0, len(lines)):
         lines[x] = replace_spelled_numbers(lines[x])
         
