47794f184119dec31773b34c5fc211090d5e7435
Oskar Deubler <deubler.oskar@gmail.com>
2023-12-01 23:04:36 +0100

    cleaned up part2.py

--- 1/part2.py
+++ 1/part2.py
@@ -1,9 +1,9 @@
-
 def read_file(filename):
     with open (filename) as input:
         lines = input.readlines()
     input.close()
     return lines
+
 def find_first_number(string):
     for x in string:
         if x.isdigit():
@@ -14,13 +14,10 @@ def get_calibration_value(string):
     first_num = find_first_number(string)
     last_num = find_first_number(reverse_string)
     number = first_num + last_num
-    #print(number)
     return int(number)
         
 def replace_spelled_numbers(string):
-    
-    #print("finding spelled numbers for " + string)
-    spelled_numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
     num_dict = {
         "one" : 1,
         "two" : 2,
@@ -33,52 +30,23 @@ def replace_spelled_numbers(string):
         "nine" : 9
     }
     
+    #Find spelled numbers in string, add
     found_dict = {}
-    indexes = []
-    for x in spelled_numbers:
+    for x in num_dict.keys():
         if x in string:
-            print(x + " found at index " + str(string.index(x)))
-            found_dict[string.index(x)] = x
-            indexes.append(string.index(x))
-
-    '''
-    if len(found_dict) > 0:
-        order = sorted(found_dict.keys())
-        string = string.replace(found_dict[order[0]], str(num_dict[found_dict[order[0]]]), 1)
-        print(string)
-    '''
-    order = sorted(found_dict.keys())
-    print(order)
-    for x in order:
-        #print(found_dict[x])
-        #print(num_dict[found_dict[x]])
-        new_substring = found_dict[x]
-        new_substring = new_substring[:1] + str(num_dict[found_dict[x]]) + new_substring[1:]
-        
-        string = string.replace(found_dict[x], new_substring)
-        
-        print(string)
-        print("")
-        
-        # e8ighto1ne8ight = eight8one1i
-    '''   
-    for x in spelled_numbers:
-        if x in string:
-            return replace_spelled_numbers(string)
-    ''' 
+            nSubstring = x
+            nSubstring = nSubstring[:1] + str(num_dict[x]) + nSubstring[1:]
+            string = string.replace(x, nSubstring)
+            
+    print(string)
     return string
 
 def main():
-
-    lines = read_file('example2.txt')
-    #lines = ["oneight"]
-
+    lines = read_file('input.txt')
+    #lines = ['oneight']
     for x in range(0, len(lines)):
-        print("------------------------------")
-        print(lines[x])
         lines[x] = replace_spelled_numbers(lines[x])
-        print(lines[x])
-        print("------------------------------")
+        
     sum = 0
     for x in lines:
         sum += get_calibration_value(x)
