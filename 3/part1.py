77deee0e26b572728555e2d0ce5597013833f959
Oskar Deubler <deubler.oskar@gmail.com>
2023-12-10 23:53:14 +0100

    cleaned up part1

--- 3/part1.py
+++ 3/part1.py
@@ -13,15 +13,6 @@ def isSymbol(ch):
         return True
     return False
 
-def get_sym_coords(lines):
-    sym_coords = []
-    for i in lines:
-        for s in symbols:
-            if s in i:
-                #print(s + " " + str(lines.index(i)) + ", "+ str(i.index(s)))
-                sym_coords.append([lines.index(i), i.index(s)])
-    return sym_coords
-
 def check_line(y, i_s, i_e, line):
     start = i_s
     end = i_e
@@ -78,31 +69,23 @@ def find_symbols_adjacent(y, i_start, i_end, lines):
 def main():
 
     lines = read_file('input.txt')
-    #print(str(len(lines)))
-    
-    #sym_coords = get_sym_coords(lines)
+
     sum = 0
     part_numbers = set()
     for y in range (0, len(lines)):
         nums = re.findall(r'\d+', lines[y])
-        #print(y)
         for i in nums:
             i_start = lines[y].index(i)
            
             i_end = lines[y].index(i) + len(i) - 1
             subs = "x" * len(i)
-            #replace the number found in the line
+            #replaces the number found in the line
             lines[y] = re.sub(i, subs, lines[y], 1)
             #print("{}, {} - {}".format(i, i_start, i_end ))
             if find_symbols_adjacent(y, i_start, i_end, lines):
                 print("{}   found @     {},{}".format(int(i), y, i_start))
-                #part_numbers.add(int(i))
                 sum = sum + int(i)
-                #35 + 592 + 664 + 598
-    #print(part_numbers)
-    """for i in part_numbers:
-        #print(i)
-        sum = sum + i"""
+
     print(sum)
 if __name__ == "__main__":
     main()
